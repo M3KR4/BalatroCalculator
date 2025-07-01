23e0589dae43ff7d88afea733c84e3d68856adec
M3KR4 <MarioMonsterMax@gmail.com>
2025-07-02 00:37:37 +0200

    Added border to clicked buttons and some other small changes

--- src/jokers.ts
+++ src/jokers.ts
@@ -1,3 +1,32 @@
+if (typeof document !== "undefined") {
+  var cardSelectionButtons = document.getElementsByClassName("cardSelectionButtons");
+  var prevClickedButton = document.getElementById("cardsButton");
+  setUpUI();
+}
+
+
+
+
+function setUpUI() {
+  for (let i = 0; i < cardSelectionButtons.length; i++) {
+    cardSelectionButtons[i]!.addEventListener("click", function (e) {
+
+      const buttonId = cardSelectionButtons[i].id;
+      const button = document.getElementById(buttonId);
+
+      if(!button) return;
+      if(!prevClickedButton) return;
+      if (prevClickedButton === button) return;
+
+      button.style.boxShadow = "inset 0 0 0 0.2vh white";
+      prevClickedButton.style.boxShadow = "none";
+      prevClickedButton = button;
+
+      return;
+
+    })
+  }
+}
 
 const jokers = {
 
@@ -31,5 +60,3 @@ const jokers = {
 
 
 };
-
-console.log(jokers.active.amounts.all);
--- web/jokers.js
+++ web/jokers.js
@@ -1,4 +1,27 @@
 "use strict";
+if (typeof document !== "undefined") {
+    var cardSelectionButtons = document.getElementsByClassName("cardSelectionButtons");
+    var prevClickedButton = document.getElementById("cardsButton");
+    setUpUI();
+}
+function setUpUI() {
+    for (let i = 0; i < cardSelectionButtons.length; i++) {
+        cardSelectionButtons[i].addEventListener("click", function (e) {
+            const buttonId = cardSelectionButtons[i].id;
+            const button = document.getElementById(buttonId);
+            if (!button)
+                return;
+            if (!prevClickedButton)
+                return;
+            if (prevClickedButton === button)
+                return;
+            button.style.boxShadow = "inset 0 0 0 0.2vh white";
+            prevClickedButton.style.boxShadow = "none";
+            prevClickedButton = button;
+            return;
+        });
+    }
+}
 const jokers = {
     active: {
         amounts: {
@@ -24,4 +47,3 @@ const jokers = {
         names: []
     }
 };
-console.log(jokers.active.amounts.all);
